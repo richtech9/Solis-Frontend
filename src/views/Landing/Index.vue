e4e19c91acb80fcd1c76169a974824bc962118d8
richtech9 <richwon426@gmail.com>
2022-05-29 20:28:42 -0300

    project
    project

---
+++ src/views/Landing/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container landing">
+    <Navbar />
+    <Hero />
+    <Lure />
+  </div>
+</template>
+
+<script>
+/* ============
+ * Home Index Page
+ * ============
+ *
+ * The home index page.
+ */
+
+import Navbar from '@/layouts/Navbar/Index.vue';
+import Hero from '@/components/Hero/Index.vue';
+import Lure from '@/components/Lure/Index.vue';
+// import Footer from '@/layouts/Footer/Index.vue';
+
+export default {
+  /**
+   * The name of the page.
+   */
+  name: 'Landing',
+
+  /**
+   * The components that the page can use.
+   */
+  components: {
+    Navbar,
+    Hero,
+    Lure,
+  },
+};
+</script>
